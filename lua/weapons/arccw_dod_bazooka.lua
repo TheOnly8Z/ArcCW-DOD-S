f1e870c2494a4e596fb8adb1a572302bfb102832
TheOnly8Z <888888zombies@gmail.com>
2020-08-14 23:22:31 -0400

    Tweak bazooka

--- lua/weapons/arccw_dod_bazooka.lua
+++ lua/weapons/arccw_dod_bazooka.lua
@@ -97,7 +97,7 @@ SWEP.KeepBaseIrons = false
 SWEP.IronSightStruct = {
     Pos = Vector(-3.74, -9, 0.1),
     Ang = Angle(3, -0.45, 0),
-    Magnification = 1.5,
+    Magnification = 1.1,
     CrosshairInSights = false
 }
 
@@ -124,9 +124,6 @@ SWEP.ActiveAng = Angle(0, 0, 0)
 SWEP.HolsterPos = Vector(0.532, -6, 0)
 SWEP.HolsterAng = Angle(-4.633, 36.881, 0)
 
-SWEP.BarrelOffsetSighted = Vector(0, 0, 0)
-SWEP.BarrelOffsetHip = Vector(3, 0, -3)
-
 SWEP.BarrelLength = 24
 
 SWEP.AttachmentElements = {
@@ -134,117 +131,99 @@ SWEP.AttachmentElements = {
         VMBodygroups = {{ind = 1, bg = 1}},
         WMBodygroups = {{ind = 1, bg = 1}},
     },
+    ["mount"] = {
+        VMElements = {
+            {
+                Model = "models/weapons/arccw/atts/mount_rail.mdl",
+                Bone = "ValveBiped.bazooka_BASE",
+                Scale = Vector(1.2, 1.2, 1.2),
+                Offset = {
+                    pos = Vector(-5.2, -0.5, -1),
+                    ang = Angle(90, 0, 0),
+                }
+            }
+        },
+        WMElements = {
+            {
+                Model = "models/weapons/arccw/atts/mount_rail.mdl",
+                Scale = Vector(-1, -1, 1),
+                Offset = {
+                    pos = Vector(1, 1, -4.5),
+                    ang = Angle(-10.393, 0, 180)
+                }
+            }
+        },
+    },
 }
+
+SWEP.ExtraSightDist = 15
 
 SWEP.Attachments = {
-{
-    PrintName = "Optic", -- print name
-    DefaultAttName = "Iron Sights",
-    Slot = {"optic_lp", "optic"}, -- what kind of attachments can fit here, can be string or table
-    Bone = "ValveBiped.bazooka_BASE", -- relevant bone any attachments will be mostly referring to
-    Offset = {
-        vpos = Vector(-5.5, -0.5, -1),
-        vang = Angle(90, 0, 0),
-        wpos = Vector(1, 1, -4.5),
-        wang = Angle(-10.393, 0, 180)
+    {
+        PrintName = "Optic", -- print name
+        DefaultAttName = "Iron Sights",
+        Slot = {"optic_lp", "optic"}, -- what kind of attachments can fit here, can be string or table
+        Bone = "ValveBiped.bazooka_BASE", -- relevant bone any attachments will be mostly referring to
+        Offset = {
+            vpos = Vector(-5.5, -0.5, -1),
+            vang = Angle(90, 0, 0),
+            wpos = Vector(1, 1, -4.5),
+            wang = Angle(-10.393, 0, 180)
+        },
+        InstalledEles = {"mount"},
     },
-},
-{
-    PrintName = "Underbarrel",
-    Slot = {"foregrip", "bipod",},
-    Bone = "ValveBiped.bazooka_BASE",
-    Offset = {
-        vpos = Vector(-2.401, 0, 8),
-        vang = Angle(90, 0, 0),
-        wpos = Vector(12, 1, -3.75),
-        wang = Angle(-10.216, 0, 180)
+    {
+        PrintName = "Underbarrel",
+        Slot = {"foregrip", "bipod",},
+        Bone = "ValveBiped.bazooka_BASE",
+        Offset = {
+            vpos = Vector(-2.401, 0, 8),
+            vang = Angle(90, 0, 0),
+            wpos = Vector(12, 1, -3.75),
+            wang = Angle(-10.216, 0, 180)
+        },
     },
-},
-{
-    PrintName = "Tactical",
-    Slot = "tac",
-    Bone = "ValveBiped.bazooka_BASE",
-    Offset = {
-        vpos = Vector(-4.4, -1, 0),
-        vang = Angle(90, 0, 90),
-        wpos = Vector(1, 2.5, -3),
-        wang = Angle(-10.393, 0, -90)
+    {
+        PrintName = "Tactical",
+        Slot = "tac",
+        Bone = "ValveBiped.bazooka_BASE",
+        Offset = {
+            vpos = Vector(-4.4, -1, 0),
+            vang = Angle(90, 0, 90),
+            wpos = Vector(1, 2.5, -3),
+            wang = Angle(-10.393, 0, -90)
+        },
     },
-},
-{
-    PrintName = "Grip",
-    Slot = "grip",
-    DefaultAttName = "Standard Grip"
-},
-{
-    PrintName = "Ammo Type",
-    Slot = "ammo_rocket"
-},
-{
-    PrintName = "Perk",
-    Slot = "perk"
-},
+    {
+        PrintName = "Grip",
+        Slot = "grip",
+        DefaultAttName = "Standard Grip"
+    },
+    {
+        PrintName = "Ammo Type",
+        Slot = "ammo_rocket"
+    },
+    {
+        PrintName = "Perk",
+        Slot = "perk"
+    },
 }
-
--- ready: deploy first time
--- draw
--- holster
--- reload
--- fire
--- fire_empty
--- cycle (for bolt actions)
--- bash
--- enter_bipod
--- exit_bipod
--- enter_sight
--- exit_sight
--- a_to_b: switch from firemode a to firemode b. e.g.: 1_to_2
--- idle
--- idle_sights
--- idle_sprint
--- idle_bipod
--- enter_inspect
--- idle_inspect
--- exit_inspect
--- append _empty for empty variation
--- use SWEP.Hook_TranslateAnimation, same as in attachment, to do even more behaviours
 
 SWEP.Animations = {
     ["idle"] = {
         Source = "idle",
         Time = 1,
     },
-    ["idle_sights"] = {
-        Source = "idle",
-        Time = 1,
-    },
-    ["idle_empty"] = {
-        Source = "idle",
-        Time = 1,
-    },
-    ["ready"] = {
+    ["draw"] = {
         Source = "draw",
         Time = 1,
         LHIK = true,
         LHIKIn = 0,
         LHIKOut = 0.25,
     },
-    ["draw"] = {
-        Source = "draw",
-        Time = 0.7,
-    },
-    ["draw_empty"] = {
-        Source = "draw",
-        Time = 0.7,
-    },
     ["fire"] = {
         Source = "launch",
-        Time = 0.6,
-    },
-    ["fire_iron"] = {
-        Source = "launch",
-        Time = 0.5,
-        ShellEjectAt = 0
+        Time = 1,
     },
     ["reload"] = {
         Source = "reload",
